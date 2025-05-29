fc220eed2dd5b50366995df3a5198fc71f614974
jtbrown21 <jt@thehypogroup.com>
2025-05-29 11:46:15 -0400

    Simplify year overview output and fix state mapping

--- src/data_health_check.py
+++ src/data_health_check.py
@@ -11,12 +11,65 @@ class SimpleDataHealthCheck:
         # All US states that should have insurance filings
         # NOTE: Florida (FL) excluded - no FL data currently available
         self.ALL_STATES = [
-            'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'GA',
-            'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
-            'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
-            'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
-            'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY', 'DC'
+            'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'GA', 'HI', 'ID',
+            'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS',
+            'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK',
+            'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV',
+            'WI', 'WY', 'DC'
         ]
+
+        self.NAME_TO_ABBR = {
+            'Alabama': 'AL',
+            'Alaska': 'AK',
+            'Arizona': 'AZ',
+            'Arkansas': 'AR',
+            'California': 'CA',
+            'Colorado': 'CO',
+            'Connecticut': 'CT',
+            'Delaware': 'DE',
+            'Georgia': 'GA',
+            'Hawaii': 'HI',
+            'Idaho': 'ID',
+            'Illinois': 'IL',
+            'Indiana': 'IN',
+            'Iowa': 'IA',
+            'Kansas': 'KS',
+            'Kentucky': 'KY',
+            'Louisiana': 'LA',
+            'Maine': 'ME',
+            'Maryland': 'MD',
+            'Massachusetts': 'MA',
+            'Michigan': 'MI',
+            'Minnesota': 'MN',
+            'Mississippi': 'MS',
+            'Missouri': 'MO',
+            'Montana': 'MT',
+            'Nebraska': 'NE',
+            'Nevada': 'NV',
+            'New Hampshire': 'NH',
+            'New Jersey': 'NJ',
+            'New Mexico': 'NM',
+            'New York': 'NY',
+            'North Carolina': 'NC',
+            'North Dakota': 'ND',
+            'Ohio': 'OH',
+            'Oklahoma': 'OK',
+            'Oregon': 'OR',
+            'Pennsylvania': 'PA',
+            'Rhode Island': 'RI',
+            'South Carolina': 'SC',
+            'South Dakota': 'SD',
+            'Tennessee': 'TN',
+            'Texas': 'TX',
+            'Utah': 'UT',
+            'Vermont': 'VT',
+            'Virginia': 'VA',
+            'Washington': 'WA',
+            'West Virginia': 'WV',
+            'Wisconsin': 'WI',
+            'Wyoming': 'WY',
+            'District of Columbia': 'DC'
+        }
 
     def check_missing_states_by_year(self, year: int):
         """Return a list of states with no filings for a specific year."""
@@ -30,7 +83,10 @@ class SimpleDataHealthCheck:
         """
         states_with_data = conn.execute(query).fetchdf()
         conn.close()
-        states_in_db = set(states_with_data['State'].tolist())
+        states_in_db = {
+            self.NAME_TO_ABBR.get(state, state)
+            for state in states_with_data['State'].tolist()
+        }
         missing_states = sorted(set(self.ALL_STATES) - states_in_db)
         return missing_states
 
@@ -99,7 +155,6 @@ class SimpleDataHealthCheck:
         ORDER BY year DESC
         """
         overview = conn.execute(query).fetchdf()
-        conn.close()
         print("\U0001F4CA DATA OVERVIEW BY YEAR")
         print("=" * 50)
         print(f"{'Year':<6} {'Filings':<12} {'States':<15}")
@@ -124,6 +179,7 @@ class SimpleDataHealthCheck:
         ORDER BY count DESC
         """
         bad_years = conn.execute(bad_years_query).fetchdf()
+        conn.close()
         if len(bad_years) > 0:
             print("\n\u26A0\uFE0F  Warning: Found filings with incorrect years:")
             for _, row in bad_years.iterrows():
@@ -174,6 +230,3 @@ class SimpleDataHealthCheck:
 if __name__ == "__main__":
     checker = SimpleDataHealthCheck('data/insurance_filings.db')
     checker.get_year_overview()
-    checker.run_health_check()
-    print("\n" + "=" * 50 + "\n")
-    checker.run_health_check(year=2024)
